81e672e0abee46674da5c95c5b0c9ae6b218efbb
WaadNajeeb <waad.najeeb@education.nsw.gov.au>
2025-09-07 09:01:18 +1000

    few

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -3,6 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000'
+  production: true,
+ apiUrl: 'https://favour-flow-tracker.onrender.com'
 };
